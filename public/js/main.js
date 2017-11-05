e3f7dc3bf5f4e1771ef3dae96c96bd718ca80b4d
philip bankier <philipbankier@gmail.com>
2017-11-04 21:55:39 -0400

    added resources page

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,11 @@ $("a[href='#top']").click(function() {
 
 //Nav bar
 $(window).scroll(function () {
-if ($(window).scrollTop() >= 50) {
-$('.navbar').css('background','rgba(101,153,255,0.75)');
-} else {
-// $('.navbar').css('background','rgba(255,255,255,0.25)');
-$('.navbar').css('background','rgba(101,153,255,0.50)');
-}
+	if ($(window).scrollTop() >= 50) {
+		$('.navbar').css('background','rgba(101,153,255,0.75)');
+	} 
+	else {
+		// $('.navbar').css('background','rgba(255,255,255,0.25)');
+		$('.navbar').css('background','rgba(101,153,255,0.50)');
+	}
 });
