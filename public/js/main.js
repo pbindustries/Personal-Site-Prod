e9fa954e7d4cc813bb4a7249193ae97ed279e06f
philip bankier <philipbankier@gmail.com>
2017-11-04 21:28:27 -0400

    Fixed navbar and changed font and background color and made it solid when not at the top of the page

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@ $("a[href='#top']").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 }); 
+
+//Nav bar
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 50) {
+$('.navbar').css('background','rgba(101,153,255,0.75)');
+} else {
+// $('.navbar').css('background','rgba(255,255,255,0.25)');
+$('.navbar').css('background','rgba(101,153,255,0.50)');
+}
+});
