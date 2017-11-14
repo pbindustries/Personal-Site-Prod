0b5b06539a78f453c9a48c36bbf359f046a1b81b
philip bankier <philipbankier@gmail.com>
2017-11-13 23:59:41 -0500

    minified css and added scss to css converter

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,81 @@
-'use strict';
-
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-gulp.task('default', ['browser-sync'], function () {
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> \n',
+  ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' */\n',
+  ''
+].join('');
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('public/sass/*.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('public/css/compiled'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
-	browserSync.init(null, {
-		proxy: "http://localhost:5000",
-        files: ["public/**/*.*"],
-		browser: ["google chrome", "firefox"],
-        // port: 7000, // Do i need this?
-        open: false, //Open browser when project is run
-	});
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('public/css/*.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public/css/minified'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+// // Minify custom JS
+// gulp.task('minify-js', function() {
+//   return gulp.src('public/js/*.js')
+//     .pipe(uglify())
+//     .pipe(header(banner, {
+//       pkg: pkg
+//     }))
+//     .pipe(rename({
+//       suffix: '.min'
+//     }))
+//     .pipe(gulp.dest('public/js/minified'))
+//     .pipe(browserSync.reload({
+//       stream: true
+//     }))
+// });
+
+// Default task
+gulp.task('default', ['sass', 'minify-css']);
+
+// Get uglify js working
+// gulp.task('default', ['sass', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+    open: false
+  })
+})
+
 gulp.task('nodemon', function (cb) {
 	
 	var started = false;
@@ -30,3 +90,19 @@ gulp.task('nodemon', function (cb) {
 	});
 });
 
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'nodemon', 'sass', 'minify-css'], function() {
+
+// Get uglify js working	
+// gulp.task('dev', ['browserSync', 'nodemon', 'sass', 'minify-css', 'minify-js'], function() {
+// 
+  gulp.watch('public/sass/*.scss', ['sass']);
+  gulp.watch('public/css/*.css', ['minify-css']);
+
+  // Get uglify js working  
+  // gulp.watch('public/js/*.js', ['minify-js']);
+  
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('public/**/*.*', browserSync.reload);
+});
